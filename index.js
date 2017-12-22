67e831993390a69a9d0775e1d7f8b440c1f56795
Miro <miroslavphoto@gmail.com>
2017-12-22 10:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const expect = require('expect');
 const fs = require('fs');
 const jsdom = require('jsdom');
 const path = require('path');
+
+describe('index', () => {
+  // there's stuff in here, too
+})
